8973665ba7e48abda0a5ebc2856fbc3d7445a925
cimbo92 <alessandro.cimbelli@mail.polimi.it>
2015-12-09 13:52:46 +0100

    Introduction of a swipe menu controller

---
+++ MyApp/MyApp/MatchOddsViewController.swift
@@ -0,0 +1,97 @@
+//
+//  MatchOddsViewController.swift
+//  MyApp
+//
+//  Created by Alessandro Cimbelli on 09/12/15.
+//  Copyright © 2015 CimboMatte. All rights reserved.
+//
+
+import UIKit
+class MatchOddsViewController: UIViewController {
+
+	var pageMenu : CAPSPageMenu?
+	
+	override func viewDidAppear(animated: Bool) {
+		super.viewDidAppear(animated)
+		
+		// MARK: - UI Setup
+		
+		self.title = "PAGE MENU"
+		self.navigationController?.navigationBar.barTintColor = UIColor(red: 30.0/255.0, green: 30.0/255.0, blue: 30.0/255.0, alpha: 1.0)
+		self.navigationController?.navigationBar.shadowImage = UIImage()
+		self.navigationController?.navigationBar.setBackgroundImage(UIImage(), forBarMetrics: UIBarMetrics.Default)
+		self.navigationController?.navigationBar.barStyle = UIBarStyle.Black
+		self.navigationController?.navigationBar.tintColor = UIColor.whiteColor()
+		self.navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.orangeColor()]
+		
+		self.navigationItem.leftBarButtonItem = UIBarButtonItem(title: "<-", style: UIBarButtonItemStyle.Done, target: self, action: "didTapGoToLeft")
+		self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "->", style: UIBarButtonItemStyle.Done, target: self, action: "didTapGoToRight")
+		
+		// MARK: - Scroll menu setup
+		
+		// Initialize view controllers to display and place in array
+		var controllerArray : [UIViewController] = []
+		
+		//Questi saranno i controller ognuno con le quote associate al suo brand
+
+		let snaiController: UIViewController = UIViewController()
+		snaiController.title = "Snai"
+		controllerArray.append(snaiController)
+		
+		let betClickController: UIViewController = UIViewController()
+		snaiController.title = "BetClik"
+		controllerArray.append(betClickController)
+		
+		let LottomaticaController: UIViewController = UIViewController()
+		snaiController.title = "Lottomatica"
+		controllerArray.append(LottomaticaController)
+		
+		
+		// Customize menu (Optional)
+		var parameters: [CAPSPageMenuOption] = [
+			.ScrollMenuBackgroundColor(UIColor(red: 30.0/255.0, green: 30.0/255.0, blue: 30.0/255.0, alpha: 1.0)),
+			.ViewBackgroundColor(UIColor(red: 20.0/255.0, green: 20.0/255.0, blue: 20.0/255.0, alpha: 1.0)),
+			.SelectionIndicatorColor(UIColor.orangeColor()),
+			.BottomMenuHairlineColor(UIColor(red: 70.0/255.0, green: 70.0/255.0, blue: 80.0/255.0, alpha: 1.0)),
+			.MenuItemFont(UIFont(name: "HelveticaNeue", size: 13.0)!),
+			.MenuHeight(40.0),
+			.MenuItemWidth(90.0),
+			.CenterMenuItems(true)
+		]
+		
+		// Initialize scroll menu
+		pageMenu = CAPSPageMenu(viewControllers: controllerArray, frame: CGRectMake(0.0, 0.0, self.view.frame.width, self.view.frame.height), pageMenuOptions: parameters)
+		
+		self.addChildViewController(pageMenu!)
+		self.view.addSubview(pageMenu!.view)
+		
+		pageMenu!.didMoveToParentViewController(self)
+	}
+	
+	func didTapGoToLeft() {
+		var currentIndex = pageMenu!.currentPageIndex
+		
+		if currentIndex > 0 {
+			pageMenu!.moveToPage(currentIndex - 1)
+		}
+	}
+	
+	func didTapGoToRight() {
+		var currentIndex = pageMenu!.currentPageIndex
+		
+		if currentIndex < pageMenu!.controllerArray.count {
+			pageMenu!.moveToPage(currentIndex + 1)
+		}
+	}
+	
+	// MARK: - Container View Controller
+	override func shouldAutomaticallyForwardAppearanceMethods() -> Bool {
+		return true
+	}
+	
+	override func shouldAutomaticallyForwardRotationMethods() -> Bool {
+		return true
+	}
+
+
+}
